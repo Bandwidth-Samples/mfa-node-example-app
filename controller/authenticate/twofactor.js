f2ddb8c361427f9625de6e534edf8caf3e8e8dfa
Peter Sherk <74555015+psherk-bw@users.noreply.github.com>
2021-01-06 15:29:38 -0500

    EDGE-607: Removed from and digits from verify request schema (#5)
    * Removed from and digits from verify request schema
    
    * EDGE-607: updating to 2.0.0
    
    * EDGE-607: Updated to remove from/to from verify and add back in to send message
    
    * EDGE-607: Updated docs

--- controller/authenticate/twofactor.js
+++ controller/authenticate/twofactor.js
@@ -14,6 +14,8 @@ twofactor.Configuration.basicAuthPassword = exampleAppConfig.apiPass;
  *   to: "string",
  *   applicationId: "string",
  *   scope: "string"
+ *   message: "string"
+ *   digits: integer
  * }
  *
  * To learn more about each field, please refer to the documentation.
@@ -40,11 +42,11 @@ const sendSmsCode = async (phoneNumber) => {
  * The structure of the object follows
  *
  * {
- *   from: "string",
  *   to: "string",
  *   applicationId: "string",
  *   scope: "string",
  *   code: "string"
+ *   expirationTimeInMinutes: integer
  * }
  *
  * To learn more about each field, please refer to the documentation.
@@ -57,12 +59,10 @@ const checkCode = async (user) => {
 
   return await controller.createVerifyTwoFactor(exampleAppConfig.accountId,
     new twofactor.TwoFactorVerifyRequestSchema({
-      from: exampleAppConfig.phoneNumber,
       to: user.phone,
       applicationId: exampleAppConfig.applicationId,
       scope: 'authorization',
       code: user.code,
-      digits: 6,
       expirationTimeInMinutes: 3
     }));
 };
